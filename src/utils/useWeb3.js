84683428fd82339c2dcb99292a132a487d72d472
aglichandrap <aglichandrap@gmail.com>
2023-03-01 20:02:54 +0700

    change rpc to mainnet

--- src/utils/useWeb3.js
+++ src/utils/useWeb3.js
@@ -1,8 +1,8 @@
 import Web3 from "web3";
 
 export function Web3Instance() {
-  const testnet = 'https://rpc.ankr.com/bsc_testnet_chapel';
+  const mainnet = 'https://wiser-dark-replica.bsc.discover.quiknode.pro/704242ae47933cc6fd6a7ea718571aed82564340/';
 
-  let instance = new Web3(testnet);
+  let instance = new Web3(mainnet);
   return instance;
 };
