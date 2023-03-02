7b8698abea0cf083c01d5b53bb0f66f6a5e2f6c1
aglichandrap <aglichandrap@gmail.com>
2023-03-02 16:27:16 +0700

    change RPC

--- src/utils/useWeb3.js
+++ src/utils/useWeb3.js
@@ -1,7 +1,7 @@
 import Web3 from "web3";
 
 export function Web3Instance() {
-  const mainnet = 'https://wiser-dark-replica.bsc.discover.quiknode.pro/704242ae47933cc6fd6a7ea718571aed82564340/';
+  const mainnet = 'https://cosmological-attentive-gadget.bsc.discover.quiknode.pro/71a7f0c442df3f18cf6281459dfc6eb845c62e12/';
 
   let instance = new Web3(mainnet);
   return instance;
